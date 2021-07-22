868cca1b48252fced9a37aef15cb55f6672c9772
Lee Scroggins <lscrggns@yahoo.com>
2021-07-22 11:09:06 -0600

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/codebreaker/CodebreakerMatchPlayApplication.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.codebreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodebreakerMatchPlayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CodebreakerMatchPlayApplication.class, args);
+  }
+
+}
---
+++ src/test/java/edu/cnm/deepdive/codebreaker/CodebreakerMatchPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.codebreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodebreakerMatchPlayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
